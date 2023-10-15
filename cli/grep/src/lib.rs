2dcf1f268bfb1ad7d22db09cd4b1c4d124102c55
Abhinav Agarwalla <120122716+ABHINAV231204@users.noreply.github.com>
2023-10-15 09:32:44 +0530

    modified grep

--- cli/grep/src/lib.rs
+++ cli/grep/src/lib.rs
@@ -45,13 +45,26 @@ pub struct  Config {
 
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return  Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        // if args.len() < 3 {
+        //     return  Err("not enough arguments");
+        // }
 
-        let query =  args[1].clone();
-        let filename = args[2].clone(); 
+        // let query =  args[1].clone();
+        // let filename = args[2].clone(); 
+
+        args.next();
+
+        let query  = match args.next() {
+            Some(arg) => arg,
+            None => return Err ("Didn't get any query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err ("Didn;'t get the file name"),
+        };
+
         //In bash ------> export CASE_INSENSITIVE=true
         // unset
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
@@ -62,14 +75,20 @@ impl Config {
 
 //Here lifetime of contents is tied to the output strings
 //Because the output lines are simply the lines from contents
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect() 
 }
 
 pub fn search_case_insensitive<'a>(
--- cli/grep/src/main.rs
+++ cli/grep/src/main.rs
@@ -5,14 +5,14 @@ use grep::Config;
 
 fn main() {
     //It takes command line as input and store them in vector args
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
 
     // ---unwrap_or_else :- ------[USED IN CASES OF RESULT ENUMS RETURNS]------
     // 1. In the Ok() case it will return the value stored in Ok.
     // 2. In the Err case ,it will execute the closure passing the error.
     // 3. Inside the closure it will printout the error and terminate the program with the status code.
 
-    let config = Config::new(&args).unwrap_or_else( |err: &str| {
+    let config = Config::new(env::args()).unwrap_or_else( |err: &str| {
         eprintln!("Problem parsing arguments : {}",err);
         process::exit(1);
     });
